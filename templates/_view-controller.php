828d4e82af6e8642e131d077de3d3141281a71e9
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-09-03 19:55:49 +0200

    clenup

--- templates/_view-controller.php
+++
@@ -1,5 +0,0 @@
-<?php namespace ProcessWire;
-// explode to get first template folder
-$inc_parts = explode("-", page('template'));
-// include folder template
-wireIncludeFile('views/' . $inc_parts[0] . '/' . page('template') . '.php');
